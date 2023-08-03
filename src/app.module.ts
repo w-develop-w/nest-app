92c2217b6a4fb89f21fc43c39bc19c872a6014a6
Danil <125916970+w-develop-w@users.noreply.github.com>
2023-08-03 07:28:01 +0300

    connecting of db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BookService } from './book/book.service';
+import { BookModule } from './book/book.module';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.DB_URI),
+    BookModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, BookService],
 })
 export class AppModule {}
