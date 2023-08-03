af9ce0e191092f224e90dc76afc6deff93b5657d
Danil <125916970+w-develop-w@users.noreply.github.com>
2023-08-03 18:22:01 +0300

    just saving (disconnecting of  'prettier' rules)

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,6 +12,8 @@ import { MongooseModule } from '@nestjs/mongoose';
       envFilePath: '.env',
       isGlobal: true,
     }),
+    // MongooseModule.forRoot(process.env.DB_URI) sets up the Mongoose module and establishes a connection to the MongoDB database using the
+    // provided URI specified in the environment variable DB_URI.
     MongooseModule.forRoot(process.env.DB_URI),
     BookModule,
   ],
